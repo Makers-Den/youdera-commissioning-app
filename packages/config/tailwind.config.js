ed0b08fb68d5528e65308b5213465c5e83f716ed
Norbert Matynia <norbert.matyniaa@gmail.com>
2022-10-19 09:40:55 +0100

    Added color palette

--- packages/config/tailwind.config.js
+++ packages/config/tailwind.config.js
@@ -7,7 +7,46 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        redtest: '#ff9966',
+        orange: {
+          DEFAULT: '#F57E02',
+          secondary: '#EA7A04',
+        },
+        green: {
+          DEFAULT: '#5CB85C',
+          secondary: '#58B158',
+        },
+        darkGray: {
+          DEFAULT: '#6D7381',
+          secondary: '#656B7A',
+        },
+        gray: {
+          DEFAULT: '#E3E6F2',
+          secondary: '#D9DDE9',
+        },
+        white: {
+          DEFAULT: '#FFFFFF',
+          secondary: '#F6F7FA',
+        },
+        purple: {
+          DEFAULT: '#7058E1',
+          secondary: '#5437D7',
+        },
+        pink: {
+          DEFAULT: '#FF504E',
+          secondary: '#F14A48',
+        },
+        darkerGray: {
+          DEFAULT: '#505364',
+          secondary: '#454856',
+        },
+        blue: {
+          DEFAULT: '#0011FF',
+          secondary: '#0614E0',
+        },
+        yellow: {
+          DEFAULT: '#FFCD00',
+          secondary: '#EFC000',
+        },
       },
       fontFamily: {
         roboto: 'Roboto',
