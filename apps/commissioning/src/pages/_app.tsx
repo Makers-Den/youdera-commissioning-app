52141fc11164db3f8986ed476063b8b8e5313c7e
kallebertell <carlpetter.bertell@gmail.com>
2022-10-19 14:06:16 +0200

    fix: type error w/ react18 & nextjs

--- apps/commissioning/src/pages/_app.tsx
+++ apps/commissioning/src/pages/_app.tsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import React, { FunctionComponent } from 'react'
 
 import { AppProps } from 'next/app'
 import '../styles/globals.css'
 
-const MyApp = ({ Component, pageProps }: AppProps) => (
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  // There's some weird type error with @react/types 18+ and NextJs
+  // https://github.com/vercel/next.js/issues/36019
+  // Working around it with a cast
+  const FixedComponent = Component as FunctionComponent;
+
   // eslint-disable-next-line react/jsx-props-no-spreading
-  <Component {...pageProps} />
-)
+  return <FixedComponent {...pageProps} />
+}
 
 export default MyApp
