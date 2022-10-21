855355b1fd1f36e8fb702292f572970f3f9d3e36
Norbert Matynia <norbert.matyniaa@gmail.com>
2022-10-21 09:31:07 +0100

    refactor: checkbox with input

--- packages/ui/src/checkboxes/Checkbox.tsx
+++ packages/ui/src/checkboxes/Checkbox.tsx
@@ -18,18 +18,23 @@ export const Checkbox: React.FC<CheckboxProps> = (
     <div className="flex items-center">
       <label className="flex relative items-center hover:cursor-pointer">
         <input type="checkbox" onClick={onClick} className='hidden' />
-        <div
-          aria-hidden="true"
-          className={`w-4 h-4 rounded-sm ${isChecked ? "bg-green" : "border-2 border-gray-secondary"}`}
-        />
-        {isChecked &&
-          <SvgIcon
-            name="Check"
+        {isChecked
+          ?
+          <span
             aria-hidden="true"
-            className="fill-white absolute -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 w-3 h-3"
+            className={`w-4 h-4 rounded-sm border-2 border-gray-secondary`}
           />
+          :
+          <span className="bg-green w-4 h-4 rounded-sm">
+            <SvgIcon
+              name="Check"
+              aria-hidden="true"
+              className="fill-white absolute -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 w-3 h-3 "
+            />
+          </span>
         }
       </label>
+
       {label &&
         <label className="text-gray-secondary ml-2">
           {label}
