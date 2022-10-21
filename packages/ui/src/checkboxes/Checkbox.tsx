9e20c2fc3a54cb21c14bbc6a0ae3a2491d877433
Norbert Matynia <norbert.matyniaa@gmail.com>
2022-10-21 09:36:35 +0100

    feat: forwarded ref

--- packages/ui/src/checkboxes/Checkbox.tsx
+++ packages/ui/src/checkboxes/Checkbox.tsx
@@ -3,43 +3,46 @@ import { SvgIcon } from "../svg-icons/SvgIcon";
 
 type CheckboxProps = {
   isChecked: boolean;
-  disabled?: boolean;
   label?: string;
   onClick: () => void;
-};
+} & React.ComponentPropsWithRef<'input'>;;
 
-export const Checkbox: React.FC<CheckboxProps> = (
-  {
-    isChecked,
-    label,
-    onClick
-  }) => {
-  return (
-    <div className="flex items-center">
-      <label className="flex relative items-center hover:cursor-pointer">
-        <input type="checkbox" onClick={onClick} className='hidden' />
-        {isChecked
-          ?
-          <span
-            aria-hidden="true"
-            className={`w-4 h-4 rounded-sm border-2 border-gray-secondary`}
-          />
-          :
-          <span className="bg-green w-4 h-4 rounded-sm">
-            <SvgIcon
-              name="Check"
+export const Checkbox = React.forwardRef<HTMLInputElement, CheckboxProps>(
+  (
+    {
+      label,
+      isChecked,
+      onClick,
+      ...rest
+    },
+    ref,
+  ) => {
+    return (
+      <div className="flex items-center">
+        <label className="flex relative items-center hover:cursor-pointer">
+          <input ref={ref} type="checkbox" onClick={onClick} className='hidden' />
+          {isChecked
+            ?
+            <span
               aria-hidden="true"
-              className="fill-white absolute -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 w-3 h-3 "
+              className={`w-4 h-4 rounded-sm border-2 border-gray-secondary`}
             />
-          </span>
-        }
-      </label>
-
-      {label &&
-        <label className="text-gray-secondary ml-2">
-          {label}
+            :
+            <span className="bg-green w-4 h-4 rounded-sm">
+              <SvgIcon
+                name="Check"
+                aria-hidden="true"
+                className="fill-white absolute -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 w-3 h-3 "
+              />
+            </span>
+          }
         </label>
-      }
-    </div>
-  );
-}
+
+        {label &&
+          <label className="text-gray-secondary ml-2">
+            {label}
+          </label>
+        }
+      </div>
+    );
+  })
