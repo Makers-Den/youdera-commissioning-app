4460e489a3103e8e48ccfe1d29572dc610f86e70
kallebertell <carlpetter.bertell@gmail.com>
2022-10-24 14:52:30 +0200

    fix: rm Foo story and fix Input story

--- apps/ui-docs/src/Input.stories.tsx
+++ apps/ui-docs/src/Input.stories.tsx
@@ -1,15 +1,11 @@
 import React from 'react';
 import { ComponentMeta, ComponentStory } from '@storybook/react';
-import { Box, BoxContent, BoxHeader, BoxTitle } from 'ui/box/Box';
-import { Button } from 'ui/buttons/Button';
 import { Input } from 'ui/inputs/Input';
-
-import { H2 } from 'ui/typography/Typography';
 
 export default {
   component: Input,
   title: 'Components/Input',
-} as ComponentMeta<typeof Box>;
+} as ComponentMeta<typeof Input>;
 
 type StoryType = ComponentStory<typeof Input>;
 
--- apps/ui-docs/src/foo/Foobar.tsx
+++
@@ -1,3 +0,0 @@
-import React from 'react';
-
-export const Foobar = ({ text }: { text: string }) => <div className="p-10 text-xxl">{text}</div>;
