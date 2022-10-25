dad5bb0ff4e18eb287c9e26a9d56862f1164ffc2
Norbert Matynia <norbert.matyniaa@gmail.com>
2022-10-25 13:58:58 +0100

    chore: playground in navheader story

--- apps/ui-docs/src/NavHeader.stories.tsx
+++ apps/ui-docs/src/NavHeader.stories.tsx
@@ -9,6 +9,12 @@ import { H3, Typography } from 'ui/typography/Typography';
 export default {
 	component: NavHeader,
 	title: 'Components/NavHeader',
+	argTypes: {
+		variant: {
+			options: ['primary', 'logo'],
+			control: { type: 'select' },
+		},
+	},
 } as ComponentMeta<typeof NavHeader>;
 
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
@@ -24,7 +30,6 @@ export const Overview = () => <div className="flex flex-col items-start gap-2">
 	<H3>Logo Variant</H3>
 	<NavHeader
 		variant='logo'
-		header='Project 1'
 		items={[
 			{
 				key: 'set',
@@ -90,4 +95,40 @@ export const Overview = () => <div className="flex flex-col items-start gap-2">
 </div>
 
 export const Playground: StoryType = Template.bind({});
-
+Playground.args = {
+	variant: 'primary',
+	header: 'Header',
+	items: [
+		{
+			key: 'set',
+			children: (
+				<button>
+					<Typography className="flex text-sm py-1">
+						<SvgIcon name="Settings" className="text-orange-400 w-5 mr-3" />
+						Setting
+					</Typography>
+				</button>
+			),
+		},
+		{
+			key: 'log',
+			children: (
+				<button>
+					<Typography className="flex text-sm py-1">
+						<SvgIcon
+							name="LogOut"
+							className="text-orange-400 w-5 mr-3"
+							color="rgb(245 126 2 / 1)"
+						/>
+						Logout
+					</Typography>
+				</button>
+			),
+		},
+	],
+	imgSrc:
+		'https://v.wpimg.pl/MTkyNjg0YjUKGzhZSEtvIElDbAMOEmF2Hlt0SEgIfWQTVigDC1U_MRsWYA0VRT01HAlgGgsfLCQCVjhbSFQkJxsVLxNIVSA2Dh1hCFNSdGxbSXtHBgB6MkZNelwDHXUwD0BjWF4FdWcPSyoOAVEudhY',
+	imgAlt: 'avatar',
+	title: 'Johny Joe',
+	subTitle: 'Roofer',
+};
