ac368bb7eb545402ce9e4c43744f09b51f2f05ca
Norbert Matynia <norbert.matyniaa@gmail.com>
2022-10-27 16:20:16 +0100

    feat: dialog component

--- packages/ui/src/box/Box.tsx
+++ packages/ui/src/box/Box.tsx
@@ -1,56 +1,52 @@
-import { ReactNode } from 'react';
-import clsxm from '../../lib/clsxm';
+import React, { ReactNode } from 'react';
+
 import { Typography } from '../typography/Typography';
+import clsxm from '../../lib/clsxm';
 
 export type BoxTitleProps = {
   title: string;
 };
 
-export function BoxTitle({ title }: BoxTitleProps) {
-  return (
-    <Typography variant="h3" weight="medium" as="h3">
-      {title}
-    </Typography>
-  );
-}
+export const BoxTitle = ({ title }: BoxTitleProps) => (
+  <Typography variant="h3" weight="medium" as="h3">
+    {title}
+  </Typography>
+)
 
 export type BoxHeaderProps = {
   children: ReactNode;
   className?: string;
 };
 
-export function BoxHeader({ className, children }: BoxHeaderProps) {
-  return (
-    <div className={clsxm('pr-8 flex items-center', className)}>
-      <div className="bg-orange-400 h-full min-h-[28px] w-1 rounded-r-full relative mr-7" />
-      {children}
-    </div>
-  );
-}
+export const BoxHeader = ({ className, children }: BoxHeaderProps) => (
+  <div className={clsxm('pr-8 flex items-center', className)}>
+    <div className="bg-orange-400 h-full min-h-[28px] w-1 rounded-r-full relative mr-7" />
+    {children}
+  </div>
+);
 
 export type BoxContentProps = {
   children: ReactNode;
   className?: string;
 };
 
-export function BoxContent({ className, children }: BoxHeaderProps) {
-  return <div className={clsxm('px-8 mt-6', className)}>{children}</div>;
-}
+export const BoxContent = ({ className, children }: BoxHeaderProps) => (
+  <div className={clsxm('px-8 mt-6', className)}>{children}</div>
+);
 
 export type BoxProps = {
   children: ReactNode;
   className?: string;
 };
 
-export function Box({ className, children }: BoxProps) {
-  return (
-    <div
-      className={clsxm(
-        'bg-white rounded-xl py-8 drop-shadow-large w-max',
-        className,
-      )}
-    >
-      {children}
-    </div>
-  );
-}
+export const Box = ({ className, children }: BoxProps) => (
+  <div
+    className={clsxm(
+      'bg-white rounded-xl py-8 drop-shadow-large w-max',
+      className,
+    )}
+  >
+    {children}
+  </div>
+);
+
