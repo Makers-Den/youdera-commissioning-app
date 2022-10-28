d7951295cd6ce4d1e63503b19bf6ab5d59e2e764
Igor Szukało <igor@makersden.io>
2022-10-28 07:36:28 +0000

    feat: added format js

--- apps/commissioning/next.config.js
+++ apps/commissioning/next.config.js
@@ -1,5 +1,9 @@
-const withTM = require('next-transpile-modules')(['ui'])
+const withTM = require('next-transpile-modules')(['ui']);
 
 module.exports = withTM({
   reactStrictMode: true,
-})
+  i18n: {
+    locales: ['en-GB', 'de-DE'],
+    defaultLocale: 'en-GB',
+  },
+});
--- apps/commissioning/src/pages/_app.tsx
+++ apps/commissioning/src/pages/_app.tsx
@@ -1,8 +1,12 @@
+import React, { FunctionComponent, useMemo } from 'react';
 import { AppProps } from 'next/app';
-import Head from 'next/head';
-import React, { FunctionComponent } from 'react';
-
 import '../styles/globals.css';
+import Head from 'next/head';
+import { useRouter } from 'next/router';
+import { IntlProvider } from 'react-intl';
+
+import English from '../../content/compiled-locales/en.json';
+import German from '../../content/compiled-locales/de.json';
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   // There's some weird type error with @react/types 18+ and NextJs
@@ -10,13 +14,34 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
   // Working around it with a cast
   const FixedComponent = Component as FunctionComponent;
 
+  const { locale } = useRouter();
+  const [shortLocale] = locale ? locale.split('-') : ['en'];
+
+  // @todo move this logic to get server side props?
+  const messages = useMemo(() => {
+    switch (shortLocale) {
+      case 'de':
+        return German;
+      case 'en':
+        return English;
+      default:
+        return English;
+    }
+  }, [shortLocale]);
+
   return (
     <>
       <Head>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      {/* eslint-disable-next-line react/jsx-props-no-spreading */}
-      <FixedComponent {...pageProps} />
+      <IntlProvider
+        locale={shortLocale}
+        messages={messages}
+        onError={() => null}
+      >
+        {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+        <FixedComponent {...pageProps} />
+      </IntlProvider>
     </>
   );
 };
