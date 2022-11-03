d9a4937ef7ce6c8b072481dcbb1f0cb9346d2dd9
Norbert Matynia <norbert.matyniaa@gmail.com>
2022-11-03 11:46:02 +0000

    chore: page setup for testing

--- apps/commissioning/src/pages/index.tsx
+++ apps/commissioning/src/pages/index.tsx
@@ -1,15 +1,20 @@
+import { AddInverterDialog } from '@src/components/add-inverter/AddInverterDialog';
 import { FieldCreationDialog } from '@src/components/field-creation/FieldCreationDialog';
 import React from 'react';
 import { Button } from 'ui/buttons/Button';
 
 
 const Home = () => {
+  const [isOpen1, setIsOpen1] = React.useState<boolean>(false)
   const [isOpen, setIsOpen] = React.useState<boolean>(false)
 
+
   return (
-    <div className="flex items-center justify-center min-h-screen space-x-8 flex-wrap">
-      <Button onClick={() => setIsOpen(true)}>Click</Button>
-      <FieldCreationDialog onClose={() => setIsOpen(false)} open={isOpen} />
+    <div className="flex flex-col items-center justify-center min-h-screen space-y-8">
+      <Button onClick={() => setIsOpen1(true)}>Create Field</Button>
+      <Button onClick={() => setIsOpen(true)}>Add Inverter</Button>
+      <FieldCreationDialog onClose={() => setIsOpen1(false)} open={isOpen1} />
+      <AddInverterDialog onClose={() => setIsOpen(false)} open={isOpen} />
     </div>
   );
 };
