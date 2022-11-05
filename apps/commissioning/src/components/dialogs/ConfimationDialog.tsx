bbe0b816c9461ea2625f8bbd9fc2ca2d14a4d094
Igor Szukalo <igor@makersden.io>
2022-11-05 13:46:27 +0100

    feat: module fields confirmation

---
+++ apps/commissioning/src/components/dialogs/ConfimationDialog.tsx
@@ -0,0 +1,69 @@
+import { useIntl } from 'react-intl';
+import { Button, ButtonProps } from 'ui/buttons/Button';
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+} from 'ui/dialogs/Dialog';
+import { SvgIcon } from 'ui/svg-icons/SvgIcon';
+import { Typography } from 'ui/typography/Typography';
+
+export type ConfimationDialogProps = {
+  isOpen: boolean;
+  onClose: () => void;
+  onCancel?: () => void;
+  onConfirm?: () => void;
+  description: string;
+  title: string;
+  isConfirming?: boolean;
+  confirmButtonVariant: ButtonProps['variant'];
+  confirmButtonTitle: string;
+};
+
+export function ConfimationDialog({
+  isOpen,
+  onClose,
+  title,
+  description,
+  onCancel,
+  onConfirm,
+  isConfirming,
+  confirmButtonVariant,
+  confirmButtonTitle,
+}: ConfimationDialogProps) {
+  const intl = useIntl();
+
+  return (
+    <Dialog open={isOpen} onClose={onClose} className="min-w-[570px]">
+      <DialogHeader>
+        <DialogTitle title={title} />
+        <SvgIcon
+          name="Close"
+          className="ml-auto h-4 hover:cursor-pointer"
+          onClick={onClose}
+        />
+      </DialogHeader>
+      <DialogContent className="flex flex-col">
+        <Typography className="mb-6">{description}</Typography>
+        <div className="flex gap-4 self-center">
+          <Button
+            variant="additional-gray"
+            onClick={onCancel}
+            className="w-[160px]"
+          >
+            {intl.formatMessage({ defaultMessage: 'Cancel' })}
+          </Button>
+          <Button
+            variant={confirmButtonVariant}
+            onClick={onConfirm}
+            className="w-[160px]"
+            isLoading={isConfirming}
+          >
+            {confirmButtonTitle}
+          </Button>
+        </div>
+      </DialogContent>
+    </Dialog>
+  );
+}
--- apps/commissioning/src/components/dialogs/DeletionDialog.tsx
+++ apps/commissioning/src/components/dialogs/DeletionDialog.tsx
@@ -1,13 +1,6 @@
 import { useIntl } from 'react-intl';
-import { Button } from 'ui/buttons/Button';
-import {
-  Dialog,
-  DialogContent,
-  DialogHeader,
-  DialogTitle,
-} from 'ui/dialogs/Dialog';
-import { SvgIcon } from 'ui/svg-icons/SvgIcon';
-import { Typography } from 'ui/typography/Typography';
+
+import { ConfimationDialog } from './ConfimationDialog';
 
 export type DeletionDialogProps = {
   isOpen: boolean;
@@ -29,37 +22,16 @@ export function DeletionDialog({
   const intl = useIntl();
 
   return (
-    <Dialog open={isOpen} onClose={onClose} className="min-w-[570px]">
-      <DialogHeader>
-        <DialogTitle
-          title={intl.formatMessage({ defaultMessage: 'Are you sure?' })}
-        />
-        <SvgIcon
-          name="Close"
-          className="ml-auto h-4 hover:cursor-pointer"
-          onClick={onClose}
-        />
-      </DialogHeader>
-      <DialogContent className="flex flex-col">
-        <Typography className="mb-6">{description}</Typography>
-        <div className="flex gap-4 self-center">
-          <Button
-            variant="additional-gray"
-            onClick={onCancel}
-            className="w-[160px]"
-          >
-            {intl.formatMessage({ defaultMessage: 'Cancel' })}
-          </Button>
-          <Button
-            variant="danger"
-            onClick={onDelete}
-            className="w-[160px]"
-            isLoading={isDeleting}
-          >
-            {intl.formatMessage({ defaultMessage: 'Delete' })}
-          </Button>
-        </div>
-      </DialogContent>
-    </Dialog>
+    <ConfimationDialog
+      isOpen={isOpen}
+      onClose={onClose}
+      title={intl.formatMessage({ defaultMessage: 'Are you sure?' })}
+      onConfirm={onDelete}
+      isConfirming={isDeleting}
+      onCancel={onCancel}
+      confirmButtonVariant="danger"
+      description={description}
+      confirmButtonTitle={intl.formatMessage({ defaultMessage: 'Delete' })}
+    />
   );
 }
