77b9188e0307e8acce5ed9cebae1ca8421b681af
kallebertell <carlpetter.bertell@gmail.com>
2022-11-10 11:05:53 +0100

    fix(Input.tsx): onFocus/Blur issues styling

--- packages/ui/src/inputs/Input.tsx
+++ packages/ui/src/inputs/Input.tsx
@@ -35,9 +35,7 @@ export const Input = React.forwardRef<HTMLInputElement, InputProps>(
     },
     ref,
   ) => {
-    const [rightElementColor, setRightElementColor] = React.useState<string>(
-      'text-gray-600 fill-gray-500',
-    );
+   
 
     let computedIcon: IconName | undefined = icon;
 
@@ -48,12 +46,6 @@ export const Input = React.forwardRef<HTMLInputElement, InputProps>(
     if (!units && validity === 'invalid') {
       computedIcon = 'Cross';
     }
-
-    const handleRightColorChange = (color: string) => {
-      if (!validity) {
-        setRightElementColor(color);
-      }
-    };
 
     return (
       <div className={clsxm(className)}>
@@ -71,42 +63,43 @@ export const Input = React.forwardRef<HTMLInputElement, InputProps>(
             ref={ref}
             type="input"
             placeholder={placeholder}
-            onFocus={() =>
-              handleRightColorChange('text-orange-400 fill-orange-400')
-            }
-            onBlur={() => handleRightColorChange('text-gray-600 fill-gray-500')}
             className={clsxm(
+              'peer/input',
               'inline-flex w-full items-center justify-center rounded px-3 py-2',
               'bg-gray-100 font-medium text-gray-800',
               'placeholder:font-normal',
               'border-[1px] border-gray-500',
               'focus:outline-none focus-visible:border-orange-400',
-              'transition-colors duration-75',
               validity && validityStyle[validity].input,
+              'transition-colors duration-75',
               'disabled:cursor-not-allowed disabled:border-gray-500 disabled:bg-gray-400 disabled:placeholder:font-medium disabled:placeholder:text-gray-800',
             )}
             {...rest}
           />
           <button
             type="button"
-            className={`absolute inset-y-0 right-0 flex items-center pr-3 text-sm ${rightElementColor} ${!onClickRightElement ? 'hover:cursor-default' : ''
-              }`}
+            className={clsxm(
+              'peer-focus/input:text-orange-400 peer-focus/input:fill-orange-400',
+              `absolute inset-y-0 right-0 flex items-center pr-3 text-sm`,
+              !onClickRightElement && 'hover:cursor-default'
+            )}
             onClick={onClickRightElement}
           >
             {computedIcon && (
               <SvgIcon
                 name={computedIcon}
                 className={clsxm(
-                  'h-4 w-4 fill-inherit',
-                  validity && validityStyle[validity].icon
+                  'h-4 w-4 fill-inherit text-inherit',
+                  validity && validityStyle[validity].icon,
                 )}
               />
             )}
             {units && (
               <span
-                className={
-                  validity ? validityStyle[validity].units : 'text-inherit'
-                }
+                className={clsxm(
+                  'text-inherit',
+                  validity && validityStyle[validity].units
+                )}
               >
                 {units}
               </span>
