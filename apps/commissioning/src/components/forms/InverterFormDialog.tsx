c0a27faba126a695ea6c2261d8cb2ca6b969af7f
Igor Szukalo <igor@makersden.io>
2022-11-15 11:16:03 +0100

    feat: added new form

---
+++ apps/commissioning/src/components/forms/InverterFormDialog.tsx
@@ -0,0 +1,164 @@
+import { zodResolver } from '@hookform/resolvers/zod';
+import React from 'react';
+import { useForm } from 'react-hook-form';
+import { useIntl } from 'react-intl';
+import { Button } from 'ui/buttons/Button';
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogProps,
+  DialogTitle,
+} from 'ui/dialogs/Dialog';
+import { Input } from 'ui/inputs/Input';
+import { SvgIcon } from 'ui/svg-icons/SvgIcon';
+import { Typography } from 'ui/typography/Typography';
+import clsxm from 'ui/utils/clsxm';
+import { z } from 'zod';
+
+import { Field } from './Field';
+import { FileField } from './FileField';
+import { Form } from './Form';
+import { InverterModelSelectFields } from './InverterModelSelectFields';
+
+const validation = z.object({
+  manufacturer: z.object({ key: z.string(), label: z.string() }),
+  model: z.object({
+    key: z.string(),
+    label: z.string(),
+    dependentKey: z.string(),
+  }),
+  serialNumber: z.string(),
+  file: z.any(),
+});
+
+type FormValues = z.infer<typeof validation>;
+
+export type InverterFormDialogProps = {
+  open: DialogProps['open'];
+  onClose: DialogProps['onClose'];
+  className?: string;
+  onSubmit: (values: FormValues, resetForm: () => void) => void;
+  title: string;
+  submitButtonTitle: string;
+};
+
+export const InverterFormDialog = ({
+  open,
+  onClose,
+  className,
+  onSubmit,
+  title,
+  submitButtonTitle,
+}: InverterFormDialogProps) => {
+  const intl = useIntl();
+
+  const method = useForm({
+    resolver: zodResolver(validation),
+  });
+
+  const { handleSubmit, reset, formState, watch } = method;
+
+  const [model, serialNumber, file] = watch(['model', 'serialNumber', 'file']);
+
+  const showFields = {
+    first: true,
+    second: !!model,
+    third: !!model && !!serialNumber,
+    fourth: !!model && !!serialNumber && !!file,
+  };
+
+  return (
+    <Dialog
+      open={open}
+      onClose={onClose}
+      className={clsxm('w-[400px]', className)}
+    >
+      <DialogHeader>
+        <DialogTitle title={title} />
+        <SvgIcon
+          name="Close"
+          className="ml-auto h-4 hover:cursor-pointer"
+          onClick={onClose}
+        />
+      </DialogHeader>
+      <DialogContent className="flex flex-col gap-5">
+        <Form
+          onSubmit={handleSubmit(values =>
+            onSubmit(values as FormValues, reset),
+          )}
+          className="flex flex-col gap-5"
+          {...method}
+        >
+          {showFields.first && <InverterModelSelectFields />}
+          {showFields.second && (
+            <Field name="serialNumber">
+              {(register, fieldState) => (
+                <Input
+                  label={intl.formatMessage({
+                    defaultMessage: 'Serial number',
+                  })}
+                  placeholder={intl.formatMessage({
+                    defaultMessage: 'S/N',
+                  })}
+                  className="w-full"
+                  {...register('serialNumber')}
+                  validity={fieldState.invalid ? 'invalid' : undefined}
+                />
+              )}
+            </Field>
+          )}
+          {showFields.third && (
+            <FileField name="file">
+              <div className="flex items-center gap-4">
+                <SvgIcon name="Camera" className="w-8 text-green-400" />
+                <div>
+                  <Typography>
+                    {intl.formatMessage({
+                      defaultMessage: 'Take photo by camera',
+                      description:
+                        'Context: Take photo by camera or click here to upload',
+                    })}
+                  </Typography>
+                  <Typography>
+                    {intl.formatMessage({
+                      defaultMessage: 'or',
+                      description:
+                        'Context: Take photo by camera or click here to upload',
+                    })}{' '}
+                    <span className="text-green-400 underline">
+                      {intl.formatMessage({
+                        defaultMessage: 'click here to upload',
+                        description:
+                          'Context: Take photo by camera or click here to upload',
+                      })}
+                    </span>
+                  </Typography>
+                </div>
+              </div>
+            </FileField>
+          )}
+          {showFields.fourth && (
+            <div className="mt-3 flex gap-5">
+              <Button
+                variant="additional-gray"
+                className="w-full"
+                onChange={onClose}
+              >
+                {intl.formatMessage({ defaultMessage: 'Cancel' })}
+              </Button>
+              <Button
+                isLoading={formState.isSubmitting}
+                type="submit"
+                variant="main-green"
+                className="w-full"
+              >
+                {submitButtonTitle}
+              </Button>
+            </div>
+          )}
+        </Form>
+      </DialogContent>
+    </Dialog>
+  );
+};
