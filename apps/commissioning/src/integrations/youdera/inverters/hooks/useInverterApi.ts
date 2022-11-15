02fbcac5edba78f230ac1001d4630f71a1d8cd7d
Igor Szukalo <igor@makersden.io>
2022-11-15 11:12:51 +0100

    feat: added new mutations

--- apps/commissioning/src/integrations/youdera/inverters/hooks/useInverterApi.ts
+++ apps/commissioning/src/integrations/youdera/inverters/hooks/useInverterApi.ts
@@ -1,5 +1,7 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 
+import { addFileToInverter } from '../mutations/addFileToInverter';
+import { createInverter } from '../mutations/createInverter';
 import { deleteInverter } from '../mutations/deleteInverter';
 import { QueryKeys } from '../../enums/queryKeys';
 
@@ -9,11 +11,27 @@ export const useInverterApi = (siteId: number) => {
   const deleteInverterMutation = useMutation(deleteInverter, {
     onSuccess: () => {
       // NOTE: this is quite coarse. We could also have inverters cached separately
-      queryClient.invalidateQueries([QueryKeys.editedSite, siteId])
+      queryClient.invalidateQueries([QueryKeys.editedSite, siteId]);
+    },
+  });
+
+  const createInverterMutation = useMutation(createInverter, {
+    onSuccess: () => {
+      // NOTE: this is quite coarse. We could also have inverters cached separately
+      queryClient.invalidateQueries([QueryKeys.editedSite, siteId]);
+    },
+  });
+
+  const addFileToInverterMutation = useMutation(addFileToInverter, {
+    onSuccess: () => {
+      // NOTE: this is quite coarse. We could also have inverters cached separately
+      queryClient.invalidateQueries([QueryKeys.editedSite, siteId]);
     },
   });
 
   return {
     deleteInverterMutation,
+    createInverterMutation,
+    addFileToInverterMutation,
   };
 };
