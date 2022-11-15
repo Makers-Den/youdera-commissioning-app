39d233a275cf571e4e46d103aa7c78fc4e44f2c9
Igor Szukalo <igor@makersden.io>
2022-11-15 13:12:58 +0100

    feat: added new mutations

--- apps/commissioning/src/integrations/youdera/batteryApiHooks.ts
+++ apps/commissioning/src/integrations/youdera/batteryApiHooks.ts
@@ -1,17 +1,22 @@
 import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 
-import { deleteBattery, executeBatteryVerification, getBatteryVerificationGuide } from './apiRequests';
+import {
+  createBattery,
+  deleteBattery,
+  executeBatteryVerification,
+  getBatteryVerificationGuide,
+} from './apiRequests';
 import { QueryKeys } from './enums/queryKeys';
 
 export const useBatteryMutations = (siteId: number) => {
   const queryClient = useQueryClient();
-  /*
+
   const createBatteryMutation = useMutation(createBattery, {
     onSuccess: () => {
-      queryClient.invalidateQueries([QueryKeys.editedSite, siteId])
+      queryClient.invalidateQueries([QueryKeys.editedSite, siteId]);
     },
   });
-
+  /*
   const updateBatteryMutation = useMutation(updateBattery, {
     onSuccess: () => {
       queryClient.invalidateQueries([QueryKeys.editedSite, siteId])
@@ -21,23 +26,27 @@ export const useBatteryMutations = (siteId: number) => {
 
   const deleteBatteryMutation = useMutation(deleteBattery, {
     onSuccess: () => {
-      queryClient.invalidateQueries([QueryKeys.editedSite, siteId])
+      queryClient.invalidateQueries([QueryKeys.editedSite, siteId]);
     },
   });
 
-  const executeBatteryVerificationMutation = useMutation(executeBatteryVerification, {
-    onSuccess: () => {
-      queryClient.invalidateQueries([QueryKeys.editedSite, siteId])
+  const executeBatteryVerificationMutation = useMutation(
+    executeBatteryVerification,
+    {
+      onSuccess: () => {
+        queryClient.invalidateQueries([QueryKeys.editedSite, siteId]);
+      },
     },
-  });
+  );
 
   return {
     deleteBatteryMutation,
     executeBatteryVerificationMutation,
+    createBatteryMutation,
   };
 };
 
-export const useBatteryVerificationGuideQuery = (id: number) => useQuery(
-  [QueryKeys.batteryVerificationGuide, id],
-  ({ queryKey }) => getBatteryVerificationGuide(queryKey[1] as number),
-);
+export const useBatteryVerificationGuideQuery = (id: number) =>
+  useQuery([QueryKeys.batteryVerificationGuide, id], ({ queryKey }) =>
+    getBatteryVerificationGuide(queryKey[1] as number),
+  );
