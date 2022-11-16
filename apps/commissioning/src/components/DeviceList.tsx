1169986fa8b742e19b695c61ff84698f5248a59b
kallebertell <carlpetter.bertell@gmail.com>
2022-11-16 16:59:06 +0100

    fix: type fixes

--- apps/commissioning/src/components/DeviceList.tsx
+++ apps/commissioning/src/components/DeviceList.tsx
@@ -1,6 +1,5 @@
 /* eslint-disable jsx-a11y/interactive-supports-focus */
-import { Battery, Meter } from '@src/integrations/youdera/apiTypes';
-import { Inverter } from '@src/integrations/youdera/inverters/types';
+import { Battery, Inverter,Meter } from '@src/integrations/youdera/apiTypes';
 import { commStatusToIcon, Device, toDevice } from '@src/utils/devices';
 import { useMemo } from 'react';
 import { useIntl } from 'react-intl';
