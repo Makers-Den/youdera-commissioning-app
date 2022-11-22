0e1e2003bc1dd2992ef3d3812a641880d7ecff21
Igor Szukalo <igor@makersden.io>
2022-11-22 17:07:55 +0100

    feat: password reset

---
+++ apps/commissioning/src/pages/forgot-password/index.tsx
@@ -0,0 +1,103 @@
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useAuth } from '@src/api/youdera/hooks/auth/hooks';
+import { Field } from '@src/components/forms/Field';
+import { Form } from '@src/components/forms/Form';
+import { LEGAL_NOTICE_URL, PRIVACY_POLICY_URL } from '@src/lib/constants';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import React from 'react';
+import { useForm } from 'react-hook-form';
+import { useIntl } from 'react-intl';
+import { Button } from 'ui/buttons/Button';
+import { Input } from 'ui/inputs/Input';
+import { Layout } from 'ui/layout/Layout';
+import { SvgIcon } from 'ui/svg-icons/SvgIcon';
+import { BodyText, H2 } from 'ui/typography/Typography';
+import { z } from 'zod';
+
+const validation = z.object({
+  email: z.string().email(),
+});
+
+type FormValues = z.infer<typeof validation>;
+
+const ForgotPassword = () => {
+  const intl = useIntl();
+
+  const router = useRouter();
+
+  const method = useForm({
+    resolver: zodResolver(validation),
+    defaultValues: { email: '' },
+  });
+
+  const { handleSubmit, formState } = method;
+
+  const { forgotPasswordMutation } = useAuth();
+
+  const onSubmit = async ({ email }: FormValues) => {
+    try {
+      await forgotPasswordMutation.mutateAsync(email);
+    } finally {
+      router.push({ pathname: '/forgot-password/success', query: { email } });
+    }
+  };
+
+  const links = [
+    {
+      name: intl.formatMessage({ defaultMessage: 'Legal notice' }),
+      href: LEGAL_NOTICE_URL,
+    },
+    {
+      name: intl.formatMessage({ defaultMessage: 'Privacy Policy' }),
+      href: PRIVACY_POLICY_URL,
+    },
+  ];
+
+  return (
+    <Layout footer={{ links }}>
+      <Form className="my-auto" onSubmit={handleSubmit(onSubmit)} {...method}>
+        <div className="flex max-w-fit flex-col items-center">
+          <SvgIcon name="Unlock" className="mb-10 h-16 w-16 fill-gray-400" />
+          <H2 className="mb-2 font-medium">
+            {intl.formatMessage({ defaultMessage: 'Forgot password?' })}
+          </H2>
+          <BodyText className="mb-8">
+            {intl.formatMessage({
+              defaultMessage: "No worries, we'll send you reset instructions.",
+            })}
+          </BodyText>
+          <Field name="email">
+            {(register, fieldState) => (
+              <Input
+                label={intl.formatMessage({ defaultMessage: 'Email' })}
+                placeholder={intl.formatMessage({
+                  defaultMessage: 'Type here',
+                })}
+                icon="Envelope"
+                className="mb-8 w-64"
+                validity={fieldState.invalid ? 'invalid' : undefined}
+                {...register('email')}
+              />
+            )}
+          </Field>
+          <Button
+            type="submit"
+            variant="main-green"
+            isLoading={formState.isSubmitting}
+            className="mb-3 w-64"
+          >
+            {intl.formatMessage({ defaultMessage: 'Reset password' })}
+          </Button>
+          <Link href="/login" passHref>
+            <Button variant="additional-gray" className="w-64">
+              {intl.formatMessage({ defaultMessage: 'Back to login' })}
+            </Button>
+          </Link>
+        </div>
+      </Form>
+    </Layout>
+  );
+};
+
+export default ForgotPassword;
--- apps/commissioning/src/pages/reset-password/index.tsx
+++ apps/commissioning/src/pages/reset-password/index.tsx
@@ -1,114 +1,143 @@
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useAuth } from '@src/api/youdera/hooks/auth/hooks';
+import { Field } from '@src/components/forms/Field';
+import { Form } from '@src/components/forms/Form';
 import { LEGAL_NOTICE_URL, PRIVACY_POLICY_URL } from '@src/lib/constants';
-import React, { useState } from 'react';
+import { useRouter } from 'next/router';
+import React, { useMemo } from 'react';
+import { useForm } from 'react-hook-form';
 import { useIntl } from 'react-intl';
 import { Button } from 'ui/buttons/Button';
 import { Input } from 'ui/inputs/Input';
 import { Layout } from 'ui/layout/Layout';
 import { SvgIcon } from 'ui/svg-icons/SvgIcon';
+import { Toast, useToast } from 'ui/toast/Toast';
 import { BodyText, H2 } from 'ui/typography/Typography';
+import { z } from 'zod';
 
-const ForgottenPassword = () => {
+export const password = z.string().min(10).max(100);
+
+const zodValidation = z.object({
+  password,
+  passwordConfirmation: password,
+});
+
+type FormValues = z.infer<typeof zodValidation>;
+
+const ResetPassword = () => {
   const intl = useIntl();
-  const [password, setPassword] = useState<string>('');
-  const [confirmPassword, setConfirmPassword] = useState<string>('');
-  const [isPasswordValid, setIsPasswordValid] = useState<
-    'valid' | 'invalid' | undefined
-  >();
-  const [nextStep, setNextStep] = useState<boolean>(false);
 
-  const handleChangePassword = (e: React.ChangeEvent<HTMLInputElement>) =>
-    setPassword(e.target.value);
+  const toast = useToast();
 
-  const handleChangeConfirmPassword = (
-    e: React.ChangeEvent<HTMLInputElement>,
-  ) => setConfirmPassword(e.target.value);
+  const validation = useMemo(
+    () =>
+      zodValidation.refine(
+        data => data.password === data.passwordConfirmation,
+        {
+          message: intl.formatMessage({
+            defaultMessage: "Passwords don't match",
+          }),
+          path: ['passwordConfirmation'],
+        },
+      ),
+    [intl],
+  );
 
-  const confirmReset = () => {
-    if (password === confirmPassword && password.length >= 8) {
-      setNextStep(true);
-    } else {
-      setIsPasswordValid('invalid');
+  const router = useRouter();
+
+  const method = useForm<FormValues>({ resolver: zodResolver(validation) });
+
+  const { handleSubmit, formState } = method;
+
+  const { resetPasswordMutation } = useAuth();
+
+  const onSubmit = async ({ password, passwordConfirmation }: FormValues) => {
+    if (router.isReady) {
+      try {
+        await resetPasswordMutation.mutateAsync({
+          password,
+          password_confirmation: passwordConfirmation,
+          token: router.query.token as string,
+          email: router.query.email as string,
+        });
+        router.push('/reset-password/success');
+      } catch (err) {
+        //@ts-ignore
+        toast.error(err.message);
+        // eslint-disable-next-line no-console
+        console.error(err);
+      }
     }
   };
-
-  //  TODO: Function for resending the emails.
 
   const links = [
     {
       name: intl.formatMessage({ defaultMessage: 'Legal notice' }),
-      href: LEGAL_NOTICE_URL
+      href: LEGAL_NOTICE_URL,
     },
     {
       name: intl.formatMessage({ defaultMessage: 'Privacy Policy' }),
-      href: PRIVACY_POLICY_URL
+      href: PRIVACY_POLICY_URL,
     },
   ];
 
   return (
-    <Layout footer={{ links }}>
-      {!nextStep ? (
-        <div className="my-auto flex max-w-fit flex-col items-center">
-          <SvgIcon name="Unlock" className="mb-10 h-16 w-16 fill-gray-400" />
-          <H2 className="mb-2 font-medium">
-            {intl.formatMessage({ defaultMessage: 'Set New Password' })}
-          </H2>
-          <BodyText className="mb-8 w-64 text-center">
-            {intl.formatMessage({
-              defaultMessage:
-                'Your password must be different to previously used passwords.',
-            })}
-          </BodyText>
-          <Input
-            label={intl.formatMessage({ defaultMessage: 'Password' })}
-            placeholder="*********"
-            type="password"
-            onChange={handleChangePassword}
-            value={password}
-            icon={isPasswordValid ? undefined : 'Unlock'}
-            className="mb-4"
-            validity={isPasswordValid}
-          />
-          <Input
-            label={intl.formatMessage({ defaultMessage: 'Confirm password' })}
-            placeholder="*********"
-            type="password"
-            onChange={handleChangeConfirmPassword}
-            value={confirmPassword}
-            icon={isPasswordValid ? undefined : 'Unlock'}
-            className="mb-8"
-            validity={isPasswordValid}
-          />
-          <Button
-            variant="main-green"
-            className="mb-3 w-64"
-            onClick={confirmReset}
-          >
-            {intl.formatMessage({ defaultMessage: 'Reset password' })}
-          </Button>
-        </div>
-      ) : (
-        <div className="mt-auto flex max-w-fit flex-col items-center">
-          <SvgIcon name="ThumbsUp" className="w-18 mb-5 h-16 fill-gray-400" />
-          <H2 className="mb-2 font-medium">
-            {intl.formatMessage({ defaultMessage: 'Reset Password' })}
-          </H2>
-          <BodyText className="mb-8 w-80 text-center">
-            {intl.formatMessage({
-              defaultMessage:
-                'Your password has been successfully reset. Click below to log in magically.',
-            })}
-          </BodyText>
-          <Button
-            variant="additional-gray"
-            className="mb-3 w-64"
-            onClick={() => undefined} // TODO: handler
-          >
-            {intl.formatMessage({ defaultMessage: 'Continue' })}
-          </Button>
-        </div>
-      )}
-    </Layout>
+    <>
+      <Layout footer={{ links }}>
+        <Form className="my-auto" onSubmit={handleSubmit(onSubmit)} {...method}>
+          <div className="flex max-w-fit flex-col items-center">
+            <SvgIcon name="Unlock" className="mb-10 h-16 w-16 fill-gray-400" />
+            <H2 className="mb-2 font-medium">
+              {intl.formatMessage({ defaultMessage: 'Set New Password' })}
+            </H2>
+            <BodyText className="mb-8 w-64 text-center">
+              {intl.formatMessage({
+                defaultMessage:
+                  'Your password must be different to previously used passwords.',
+              })}
+            </BodyText>
+            <Field name="password">
+              {(register, fieldState) => (
+                <Input
+                  label={intl.formatMessage({ defaultMessage: 'Password' })}
+                  placeholder="*********"
+                  type="password"
+                  icon={fieldState.invalid ? 'Unlock' : undefined}
+                  className="mb-4"
+                  validity={fieldState.invalid ? 'invalid' : undefined}
+                  {...register('password')}
+                />
+              )}
+            </Field>
+            <Field name="passwordConfirmation">
+              {(register, fieldState) => (
+                <Input
+                  label={intl.formatMessage({
+                    defaultMessage: 'Confirm password',
+                  })}
+                  placeholder="*********"
+                  type="password"
+                  icon={fieldState.invalid ? 'Unlock' : undefined}
+                  className="mb-8"
+                  validity={fieldState.invalid ? 'invalid' : undefined}
+                  {...register('passwordConfirmation')}
+                />
+              )}
+            </Field>
+            <Button
+              variant="main-green"
+              isLoading={formState.isSubmitting}
+              className="mb-3 w-64"
+              type="submit"
+            >
+              {intl.formatMessage({ defaultMessage: 'Reset password' })}
+            </Button>
+          </div>
+        </Form>
+      </Layout>
+      <Toast />
+    </>
   );
 };
 
-export default ForgottenPassword;
+export default ResetPassword;
