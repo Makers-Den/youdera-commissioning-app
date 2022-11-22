48116fca52bdb0868a75a7de2f7e791d722abb5a
Norbert Matynia <norbert.matyniaa@gmail.com>
2022-11-22 15:21:42 +0000

    refactor: multi select

--- apps/commissioning/src/components/forms/MultiSelectField.tsx
+++ apps/commissioning/src/components/forms/MultiSelectField.tsx
@@ -1,21 +1,21 @@
 import { useController } from 'react-hook-form';
-import { MultiSelect, MultiSelectProps } from 'ui/select/MultiSelect';
+import { MultiSelect, MultiSelectProps, MultiSelectValue } from 'ui/select/MultiSelect';
 
 import { Field } from './Field';
 
-export type MultiSelectFieldProps = {
+export type MultiSelectFieldProps<Value extends MultiSelectValue> = {
   name: string;
-} & Omit<MultiSelectProps, 'value' | 'onChange' | 'name' | 'validity'>;
+} & Omit<MultiSelectProps<Value>, 'value' | 'onChange' | 'name' | 'validity'>;
 
-export function MultiSelectField({ name, ...props }: MultiSelectFieldProps) {
+export function MultiSelectField<Value extends MultiSelectValue>({ name, ...props }: MultiSelectFieldProps<Value>) {
   const control = useController({ name });
-
   return (
     <Field name={name}>
       {(_, fieldState) => (
         <MultiSelect
           {...props}
           {...control.field}
+          value={control.field.value || []}
           validity={fieldState.invalid ? 'invalid' : undefined}
         />
       )}
