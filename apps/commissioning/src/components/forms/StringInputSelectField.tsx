7a2b10da22eae2920fa0d61a93aba534f8270dbc
Norbert Matynia <norbert.matyniaa@gmail.com>
2022-11-25 15:57:16 +0000

    refactor: code, queries into seprate file and naming

---
+++ apps/commissioning/src/components/forms/StringInputSelectField.tsx
@@ -0,0 +1,57 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+import { Inverter } from '@src/api/youdera/apiTypes';
+import { useMemo } from 'react';
+import { useIntl } from 'react-intl';
+import { IconName } from 'ui/svg-icons/SvgIcon';
+
+import {
+  DependentSelectsField,
+} from './DependentSelectsField';
+
+export type StringInputSelectFieldProps = {
+  inverterValue: any;
+  inverters: Inverter[];
+};
+
+export const StringInputSelectField = ({
+  inverterValue,
+  inverters
+}: StringInputSelectFieldProps) => {
+  const intl = useIntl();
+
+  const inverterInputsOptions = useMemo(() => {
+    if (!inverterValue?.key || inverterValue?.key === '-1') return [];
+
+    const selectedInverter = inverters.filter(
+      inverter => inverter.id.toString() === inverterValue.key,
+    )[0];
+
+    return selectedInverter.mpp_trackers.map((input, idx) => ({
+      children: () => (idx + 1).toString(),
+      value: {
+        key: input.id.toString(),
+        label: (idx + 1).toString(),
+        value: input.id,
+        dependentKey: inverterValue?.key ?? '',
+      },
+      icon: 'Chip' as IconName,
+    }));
+  }, [inverterValue, inverters]);
+
+  return (
+    <DependentSelectsField
+      value={inverterValue}
+      name="inverter"
+      dependentOptions={inverterInputsOptions}
+      dependentSelectProps={{
+        label: intl.formatMessage({
+          defaultMessage: 'Input',
+        }),
+        placeholder: intl.formatMessage({
+          defaultMessage: 'Select',
+        }),
+      }}
+      dependentName="input"
+    />
+  );
+};
--- apps/commissioning/src/components/forms/StringInverterDialog.tsx
+++ apps/commissioning/src/components/forms/StringInverterDialog.tsx
@@ -1,7 +1,6 @@
 import { zodResolver } from '@hookform/resolvers/zod';
-import { ApiFile, Inverter, InverterModel } from '@src/api/youdera/apiTypes';
+import { ApiFile, Inverter } from '@src/api/youdera/apiTypes';
 import {
-  useInverterModelsQuery,
   useInvertersQuery,
 } from '@src/api/youdera/hooks/inverters/hooks';
 import React, { useEffect, useMemo, useState } from 'react';
@@ -22,10 +21,10 @@ import clsxm from 'ui/utils/clsxm';
 import { z, ZodObject, ZodTypeAny } from 'zod';
 
 import { AutocompleteSelectField } from './AutocompleteField';
-import { ExistingInverterSelectField } from './ExistingInverterSelectField';
 import { FileField } from './FileField';
 import { Form } from './Form';
-import { NewInverterSelectField } from './NewInverterSelectField';
+import { StringInputSelectField } from './StringInputSelectField';
+import { StringModelInputSelectField } from './StringModelInputSelectField';
 
 type RawFormShapeExistingInverter = {
   inverter: ZodTypeAny;
@@ -108,12 +107,9 @@ export const StringInverterDialog = <
     ),
   });
   const { handleSubmit, reset, formState, watch, control } = method;
-  const inverterModelsQuery = useInverterModelsQuery();
-  const inverterModels = inverterModelsQuery.data as InverterModel[];
   const invertersQuery = useInvertersQuery(siteId);
   const inverters = invertersQuery.data as Inverter[];
 
-  // * Intersection input
   const inverterOptions: AutocompleteSelectOption[] | [] = useMemo(
     () =>
       [
@@ -134,7 +130,6 @@ export const StringInverterDialog = <
     // eslint-disable-next-line react-hooks/exhaustive-deps
     [inverters],
   );
-  // *
 
   const watchInverter = useWatch({
     control,
@@ -152,79 +147,6 @@ export const StringInverterDialog = <
     if (watchInverter) setIsWithNewInverter(watchInverter.key === '-1');
   }, [watchInverter]);
 
-  const inverterInputsOptions = useMemo(() => {
-    if (!watchInverter?.key || watchInverter?.key === '-1') return [];
-
-    const selectedInverter = inverters.filter(
-      inverter => inverter.id.toString() === watchInverter.key,
-    )[0];
-
-    return selectedInverter.mpp_trackers.map((input, idx) => ({
-      value: {
-        key: input.id.toString(),
-        label: (idx + 1).toString(),
-        value: input.id,
-        dependentKey: watchInverter?.key ?? '',
-      },
-      icon: 'Chip' as IconName,
-      children: () => (idx + 1).toString(),
-    }));
-  }, [watchInverter, inverters]);
-
-  // * Form with creation of new inverter
-  const inverterManufacturersOptions = useMemo(() => {
-    const result: unknown[] = [];
-    const map = new Map();
-    inverterModels.forEach(model => {
-      if (!map.has(model.manufacturer_id)) {
-        map.set(model.manufacturer_id, true);
-        result.push({
-          key: model.manufacturer_id.toString(),
-          label: model.manufacturer_name,
-          value: model.manufacturer_id,
-          dependentKey: (watchInverter as AutocompleteSelectOption)?.key ?? '',
-        });
-      }
-    });
-    return result;
-  }, [inverterModels, watchInverter]);
-
-  const inverterModelsOptions = useMemo(() => {
-    if (!watchManufacturer || !inverterModels) return [];
-    return inverterModels
-      .filter(
-        //@ts-ignore
-        model => model.manufacturer_id.toString() === watchManufacturer.key,
-      )
-      .map(model => ({
-        key: model.id.toString(),
-        label: model.name,
-        value: model.id,
-        icon: 'Table',
-        //@ts-ignore
-        dependentKey: watchManufacturer.key,
-      }));
-  }, [inverterModels, watchManufacturer]);
-
-  const inverterNewInputsOptions = useMemo(() => {
-    if (!watchModel || !inverterModels) return [];
-    const numberOfInputs = inverterModels.filter(
-      //@ts-ignore
-      model => model.id.toString() === watchModel.key,
-    )[0]?.data.inputs;
-    return Array(numberOfInputs)
-      .fill(0)
-      .map((_, idx) => ({
-        key: idx.toString(),
-        label: (idx + 1).toString(),
-        icon: 'Chip',
-        value: idx.toString(),
-        //@ts-ignore
-        dependentKey: watchModel.key,
-      }));
-  }, [inverterModels, watchModel]);
-  // *
-
   return (
     <Dialog
       open={open}
@@ -236,11 +158,11 @@ export const StringInverterDialog = <
           title={
             modifiedStringId
               ? intl.formatMessage({
-                  defaultMessage: 'Modify String',
-                })
+                defaultMessage: 'Modify String',
+              })
               : intl.formatMessage({
-                  defaultMessage: 'Add String',
-                })
+                defaultMessage: 'Add String',
+              })
           }
         />
         <SvgIcon
@@ -271,76 +193,75 @@ export const StringInverterDialog = <
             options={inverterOptions}
           />
           {watchInverter?.key !== '-1' ? (
-            <ExistingInverterSelectField
+            <StringInputSelectField
               inverterValue={watchInverter}
-              inverterInputsOptions={inverterInputsOptions}
+              inverters={inverters}
             />
-          ) : null
-          // <NewInverterSelectField
-          //   inverterValue={watchInverter}
-          //   inverterManufacturerOptions={inverterManufacturersOptions}
-          //   inverterModelOptions={inverterModelsOptions}
-          //   inverterInputsOptions={inverterNewInputsOptions}
-          // />
-          }
-          {((watchInverter.key !== '-1' && watchInput) ||
-            (watchInverter.key === '-1' && watchNewInput)) && (
-            <FileField
-              className="w-full"
-              label={intl.formatMessage({
-                defaultMessage: 'String test result',
-              })}
-              name="file"
-              valueMapper={fileValueMapper}
-            >
-              <div className="flex items-center gap-4">
-                <SvgIcon name="Camera" className="w-8 text-green-400" />
-                <div>
-                  <Typography>
-                    {intl.formatMessage({
-                      defaultMessage: 'Take photo by camera',
-                      description:
-                        'Context: Take photo by camera or click here to upload',
-                    })}
-                  </Typography>
-                  <Typography>
-                    {intl.formatMessage({
-                      defaultMessage: 'or',
-                      description:
-                        'Context: Take photo by camera or click here to upload',
-                    })}{' '}
-                    <span className="text-green-400 underline">
+          ) : (
+            <StringModelInputSelectField
+              inverterValue={watchInverter}
+              manufacturerValue={watchManufacturer}
+              modelValue={watchModel}
+            />
+          )}
+          {((watchInverter?.key !== '-1' && watchInput) ||
+            (watchInverter?.key === '-1' && watchNewInput)) && (
+              <FileField
+                className="w-full"
+                label={intl.formatMessage({
+                  defaultMessage: 'String test result',
+                })}
+                name="file"
+                valueMapper={fileValueMapper}
+              >
+                <div className="flex items-center gap-4">
+                  <SvgIcon name="Camera" className="w-8 text-green-400" />
+                  <div>
+                    <Typography>
                       {intl.formatMessage({
-                        defaultMessage: 'click here to upload',
+                        defaultMessage: 'Take photo by camera',
                         description:
                           'Context: Take photo by camera or click here to upload',
                       })}
-                    </span>
-                  </Typography>
+                    </Typography>
+                    <Typography>
+                      {intl.formatMessage({
+                        defaultMessage: 'or',
+                        description:
+                          'Context: Take photo by camera or click here to upload',
+                      })}{' '}
+                      <span className="text-green-400 underline">
+                        {intl.formatMessage({
+                          defaultMessage: 'click here to upload',
+                          description:
+                            'Context: Take photo by camera or click here to upload',
+                        })}
+                      </span>
+                    </Typography>
+                  </div>
                 </div>
+              </FileField>
+            )}
+          {((watchInverter?.key !== '-1' && watchInput && watchFile) ||
+            (watchInverter?.key === '-1' && watchNewInput && watchFile)) && (
+              <div className="mt-3 flex gap-5">
+                <Button
+                  variant="additional-gray"
+                  className="w-full"
+                  onClick={() => onClose(reset)}
+                >
+                  {intl.formatMessage({ defaultMessage: 'Cancel' })}
+                </Button>
+                <Button
+                  variant="main-green"
+                  className="w-full"
+                  type="submit"
+                  isLoading={formState.isSubmitting}
+                >
+                  {intl.formatMessage({ defaultMessage: 'Ok' })}
+                </Button>
               </div>
-            </FileField>
-          )}
-          {((watchInverter.key !== '-1' && watchInput && watchFile) ||
-            (watchInverter.key === '-1' && watchNewInput && watchFile)) && (
-            <div className="mt-3 flex gap-5">
-              <Button
-                variant="additional-gray"
-                className="w-full"
-                onClick={() => onClose(reset)}
-              >
-                {intl.formatMessage({ defaultMessage: 'Cancel' })}
-              </Button>
-              <Button
-                variant="main-green"
-                className="w-full"
-                type="submit"
-                isLoading={formState.isSubmitting}
-              >
-                {intl.formatMessage({ defaultMessage: 'Ok' })}
-              </Button>
-            </div>
-          )}
+            )}
         </Form>
       </DialogContent>
     </Dialog>
