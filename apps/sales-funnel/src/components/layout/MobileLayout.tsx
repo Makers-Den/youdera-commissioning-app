1c0500414d29d198ba7b1ccb7c9d8d6610849a79
Norbert Matynia <norbert.matyniaa@gmail.com>
2023-10-04 15:16:52 +0200

    refactor: enhanced responsiveness of clip path

--- apps/sales-funnel/src/components/layout/MobileLayout.tsx
+++ apps/sales-funnel/src/components/layout/MobileLayout.tsx
@@ -36,35 +36,19 @@ export const MobileLayout = ({
   </body>
 );
 
-// TODO ? refactor clip paths
 const ClipPaths = () => (
-  <>
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 390 71"
-      fill="none"
-      className="absolute top-0 w-full"
-    >
-      <clipPath id="roundedClip">
-        <path
-          d="M0 0H390V71C279.5 59.3987 107.5 59.3987 0 71V0Z"
-          fill="#EA7A04"
-        />
-      </clipPath>
-    </svg>
-    <svg
-      className="absolute top-0 w-full"
-      xmlns="http://www.w3.org/2000/svg"
-      height="75"
-      viewBox="0 0 640 75"
-      fill="none"
-    >
-      <clipPath id="roundedClipLarge">
-        <path
-          d="M0 0H640V75C458.667 62.7451 176.41 62.7451 0 75V0Z"
-          fill="#EA7A04"
-        />
-      </clipPath>
-    </svg>
-  </>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 390 71"
+    fill="none"
+    className="absolute top-0 h-0 w-0"
+  >
+    <clipPath id="roundedClip" clipPathUnits="objectBoundingBox">
+      <path
+        transform="scale(0.0025641,0.0140845)"
+        d="M0 0H390V71C279.5 59.3987 107.5 59.3987 0 71V0Z"
+        fill="#EA7A04"
+      />
+    </clipPath>
+  </svg>
 );
