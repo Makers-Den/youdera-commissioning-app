de4083764cdb64724581b2e3f1fb984340c5a92c
Norbert Matynia <norbert.matyniaa@gmail.com>
2023-10-05 09:19:40 +0200

    feat: grid container

--- apps/sales-funnel/src/app/page.tsx
+++ apps/sales-funnel/src/app/page.tsx
@@ -1,13 +1,13 @@
+import { Container } from '@src/components/container/Container';
 import { Button } from 'ui/buttons/Button';
 import { BodyText } from 'ui/typography/Typography';
 
-import { MobileLayout } from '../components/layout/MobileLayout';
 import { HandWithPhoneSvg } from '../components/svgs/HandWithPhoneSvg';
 
 export default function Home() {
   return (
-    <MobileLayout clipped title="Consult with sales">
-      <div className="flex h-full flex-col  justify-between bg-white p-5">
+    <Container clippedTitle title="Consult with sales">
+      <div className="flex flex-1 flex-col justify-between bg-white p-5">
         <div className="flex flex-col items-center gap-20">
           <BodyText className="text-black">
             We’re not able to give you an automated offer for the given choice,
@@ -22,6 +22,6 @@ export default function Home() {
           </Button>
         </div>
       </div>
-    </MobileLayout>
+    </Container>
   );
 }
