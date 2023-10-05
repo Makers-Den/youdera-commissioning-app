4a7915acec2a26a1fd7258bc7901b16371d03786
Norbert Matynia <norbert.matyniaa@gmail.com>
2023-10-05 14:22:56 +0200

    feat: custom radio group

--- apps/sales-funnel/src/page-components/Development.tsx
+++ apps/sales-funnel/src/page-components/Development.tsx
@@ -2,10 +2,12 @@
 
 import { Container } from '@src/components/container/Container';
 import React from 'react';
+import { CustomRadioGroup } from 'ui/radio-group/CustomRadioGroup';
 import { RadioGroup } from 'ui/radio-group/RadioGroup';
+import { SvgIcon } from 'ui/svg-icons/SvgIcon';
 
 const options = [
-  { name: 'welcome', value: 'welcome' },
+  { name: 'welcome', value: 'welcome', element: <SvgIcon name="Calendar" /> },
   { name: '1', value: '1' },
   { name: '2', value: '2' },
 ];
@@ -15,6 +17,14 @@ export const Development = () => {
   return (
     <Container title="Development Page">
       <RadioGroup
+        options={options}
+        selected={selected}
+        onChange={e => {
+          setSelected(e);
+        }}
+      />
+
+      <CustomRadioGroup
         options={options}
         selected={selected}
         onChange={e => {
--- packages/ui/src/radio-group/CustomRadioGroup.tsx
+++ packages/ui/src/radio-group/CustomRadioGroup.tsx
@@ -1,12 +1,13 @@
 import { RadioGroup as Radio } from '@headlessui/react';
 import clsxm from 'ui/utils/clsxm';
 
-import { BodyText } from '../typography/Typography';
+import { NoteText } from '../typography/Typography';
 
-type Option = { name: string; value: string };
+type Option = { name: string; value: string; element: React.ReactNode };
 
 type RadioGroupProps = {
   className?: string;
+  optionClassName?: string;
   label?: string;
   options: Option[];
   defaultOption?: Option;
@@ -14,45 +15,39 @@ type RadioGroupProps = {
   selected: Option | undefined;
 };
 
-const RadioButton = ({ checked }: { checked: boolean }) => (
-  <div
-    className={clsxm(
-      'h-6 w-6 border border-gray-500 bg-gray-100',
-      'rounded-full',
-      checked && 'border-brand-one-400  border-[0.375rem] ',
-    )}
-  />
-);
-
-export const RadioGroup: React.FC<RadioGroupProps> = ({
+export const CustomRadioGroup: React.FC<RadioGroupProps> = ({
   options,
   label,
   onChange,
   selected,
+  className,
+  optionClassName,
 }) => (
   <div className="w-full px-4 py-16">
     <div className="mx-auto w-full max-w-md">
       <Radio value={selected} onChange={(value: Option) => onChange(value)}>
         <Radio.Label className="sr-only">{label}</Radio.Label>
-        <div className="space-y-2">
+        <div className={clsxm('grid grid-cols-3 gap-4', className)}>
           {options.map(option => (
             <Radio.Option
               key={option.name}
               value={option.value}
               className={({ checked }) =>
                 clsxm(
-                  'relative flex cursor-pointer rounded-md border border-gray-600 px-5 py-4 focus:outline-none',
+                  'relative col-span-1 w-full  cursor-pointer rounded-md border border-gray-600 px-5 py-4 focus:outline-none',
+                  'text-gray-1000 flex flex-col items-center justify-center gap-2',
                   checked && 'border-brand-one-400 border',
+                  optionClassName,
                 )
               }
             >
               {({ checked }) => (
-                <div className="flex w-full items-center gap-3">
-                  <RadioButton checked={checked} />
-                  <BodyText className="text-gray-1000 text-sm">
-                    {option.name}
-                  </BodyText>
-                </div>
+                <>
+                  {option.element}
+                  <Radio.Label as="p" className="text-sm">
+                    <NoteText>{option.name}</NoteText>
+                  </Radio.Label>
+                </>
               )}
             </Radio.Option>
           ))}
